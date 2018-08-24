183841518db6616de8256054a3da8f4311d119ec
Nhan Huynh <>
2018-08-24 11:04:58 -0500

    Flare VM 2.0
    - Create a new package: flarevm.installer.flare: This package
      contains an install script, an uninstall script, and a list of
      all packages to be installed. This allow chocolatey to manage
      install, uninstall and update properly
    
    - Clean up install.ps1 script to add the FLARE feed on myget.org
    
    - Remove legacy install.bat script.
    
    - Update contact information to flarevm@fireeye.com.

--- install.ps1
+++ install.ps1
@@ -18,27 +18,40 @@ Write-Host " |_|    |______/_/    \_\_|  \_\______|      \/   |_|  |_|"
 Write-Host "                   I N S T A L L A T I O N                "
 Write-Host "  ________________________________________________________"
 Write-Host "                         Developed by                     "
-Write-Host "                      Peter Kacherginsky                  "
 Write-Host "       FLARE (FireEye Labs Advanced Reverse Engineering)  "
+Write-Host "                     flarevm@fireeye.com                  "
 Write-Host "  _______________________________________________________ "
 Write-Host "                                                          "
 
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
 
+
 if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
   Write-Host "[ * ] Installing Boxstarter"
   iex ((New-Object System.Net.WebClient).DownloadString('http://boxstarter.org/bootstrapper.ps1')); get-boxstarter -Force
 
+
+  # Boxstarter options
+  $Boxstarter.RebootOk=$true # Allow reboots?
+  $Boxstarter.NoPassword=$false # Is this a machine with no login password?
+  $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+  Set-BoxstarterConfig -NugetSources "https://www.myget.org/F/flare/api/v2;https://chocolatey.org/api/v2"
+
+
   # Get user credentials for autologin during reboots
   Write-Host "[ * ] Getting user credentials ..."
-
   Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
   $cred=Get-Credential $env:username
 
+
+  # Needed for many applications
+  cinst -y vcredist-all
+
+
   if ($cred) {
-      Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/fireeye/flare-vm/master/flarevm_malware.ps1 -Credential $cred
+      Install-BoxstarterPackage -PackageName flarevm.installer.flare -Credential $cred
   } else {
-      Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/fireeye/flare-vm/master/flarevm_malware.ps1
+      Install-BoxstarterPackage -PackageName flarevm.installer.flare
   }
 
 } else {
