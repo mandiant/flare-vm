2220abf352ad0f0806ba92361d63028287184661
htnhan <huynh.t.nhan@gmail.com>
2018-10-03 14:37:34 -0400

    reorganize install.ps1, it's growing too large

--- install.ps1
+++ install.ps1
@@ -9,73 +9,90 @@
 #
 ###########################################
 
+
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
 
-if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+if (-Not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "[ERR] Please run this script as administrator"
+	Read-Host  "      Press ANY key to continue..."
+	exit
+}
 
-# https://stackoverflow.com/questions/11696944/powershell-v3-invoke-webrequest-https-error
-# Allows current PowerShell session to trust all certificates
-# Also a good find: https://www.briantist.com/errors/could-not-establish-trust-relationship-for-the-ssltls-secure-channel/
-  Add-Type @"
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
+
+function installBoxStarter()
+{
+	# https://stackoverflow.com/questions/11696944/powershell-v3-invoke-webrequest-https-error
+	# Allows current PowerShell session to trust all certificates
+	# Also a good find: https://www.briantist.com/errors/could-not-establish-trust-relationship-for-the-ssltls-secure-channel/
+	Add-Type @"
+	using System.Net;
+	using System.Security.Cryptography.X509Certificates;
+	public class TrustAllCertsPolicy : ICertificatePolicy {
+		public bool CheckValidationResult(
+			ServicePoint srvPoint, X509Certificate certificate,
+			WebRequest request, int certificateProblem) {
+			return true;
+		}
+	}
 "@
-  
-  try {
-    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
-  } catch {
-    Write-Debug "Failed to find SSL type...1"
-  }
-  try {
-    $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls'
-  } catch {
-    Write-Debug "Failed to find SSL type...2"
-  }
-  
-  $prevSecProtocol = [System.Net.ServicePointManager]::SecurityProtocol
-  $prevCertPolicy = [System.Net.ServicePointManager]::CertificatePolicy
-  
-  Write-Host "[ * ] Installing Boxstarter"
-  # Become overly trusting
-  [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-  [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-  iex ((New-Object System.Net.WebClient).DownloadString('http://boxstarter.org/bootstrapper.ps1')); get-boxstarter -Force
-  # Restore previous trust settings for this PowerShell session
-  # Note: SSL certs trusted from installing BoxStarter above will be trusted for the remaining PS session
-  [System.Net.ServicePointManager]::SecurityProtocol = $prevSecProtocol
-  [System.Net.ServicePointManager]::CertificatePolicy = $prevCertPolicy
 
-  # Boxstarter options
-  $Boxstarter.RebootOk=$true # Allow reboots?
-  $Boxstarter.NoPassword=$false # Is this a machine with no login password?
-  $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
-  Set-BoxstarterConfig -NugetSources "https://www.myget.org/F/flare/api/v2;https://chocolatey.org/api/v2" -LocalRepo "."
+	try {
+		$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+	} catch {
+		Write-Debug "Failed to find SSL type...1"
+	}
+	
+	try {
+		$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls'
+	} catch {
+		Write-Debug "Failed to find SSL type...2"
+	}
 
-  # Go ahead and disable the Windows Updates
-  Disable-MicrosoftUpdate
+	$prevSecProtocol = [System.Net.ServicePointManager]::SecurityProtocol
+	$prevCertPolicy = [System.Net.ServicePointManager]::CertificatePolicy
 
-  # Get user credentials for autologin during reboots
-  Write-Host "[ * ] Getting user credentials ..."
-  Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
-  $cred=Get-Credential $env:username
+	Write-Host "[ * ] Installing Boxstarter"
+	# Become overly trusting
+	[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+	[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+	
+	# download and instal boxstarter
+	iex ((New-Object System.Net.WebClient).DownloadString('http://boxstarter.org/bootstrapper.ps1')); get-boxstarter -Force
+	
+	# Restore previous trust settings for this PowerShell session
+	# Note: SSL certs trusted from installing BoxStarter above will be trusted for the remaining PS session
+	[System.Net.ServicePointManager]::SecurityProtocol = $prevSecProtocol
+	[System.Net.ServicePointManager]::CertificatePolicy = $prevCertPolicy
+	return $true
 
-  # Needed for many applications
-  cinst -y vcredist-all
+}
 
-  if ($cred) {
-      Install-BoxstarterPackage -PackageName flarevm.installer.flare -Credential $cred
-  } else {
-      Install-BoxstarterPackage -PackageName flarevm.installer.flare
-  }
+$rc = installBoxStarter
+if ( -Not $rc ) {
+	Write-Host "[ERR] Failed to install BoxStarter"
+	Read-Host  "      Press ANY key to continue..."
+	exit
+}
 
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+Set-BoxstarterConfig -NugetSources "https://www.myget.org/F/flare/api/v2;https://chocolatey.org/api/v2" -LocalRepo "."
+
+# Go ahead and disable the Windows Updates
+Disable-MicrosoftUpdate
+
+# Get user credentials for autologin during reboots
+Write-Host "[ * ] Getting user credentials ..."
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
+$cred=Get-Credential $env:username
+
+# Needed for many applications
+cinst -y vcredist-all
+
+if ($cred) {
+  Install-BoxstarterPackage -PackageName flarevm.installer.flare -Credential $cred
 } else {
-  Write-Host "[ERR] Please run this script as administrator"
-  Read-Host  "      Press ANY key to continue..."
+  Install-BoxstarterPackage -PackageName flarevm.installer.flare
 }
