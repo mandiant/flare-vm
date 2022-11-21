85209ecf38b3e64a1eb363495ca801cf2208d1e2
MalwareMechanic <malwaremechanic@gmail.com>
2022-11-20 21:38:22 -0500

    Removed deprecated files and folders

--- flarevm.config.flare/tools/chocolateyuninstall.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$packageName = 'flarevm.config.flare'
-
-try {
-	$desktopReadme = Join-Path ${Env:UserProfile} "Desktop\README.txt"
-	Remove-Item $desktopReadme
-} catch {
-	# pass
-}
