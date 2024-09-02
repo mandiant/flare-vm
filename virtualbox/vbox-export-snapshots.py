dcbedd34b45ce923860bdef6d7d6d3a6e9327b89
Ana Maria Martinez Gomez <anamg@google.com>
2024-09-02 18:14:04 +0200

    [vbox-export-snapshots] Create manifest
    Use the `create_manifest` export option that writes the optional
    manifest file (`.mf`) which is used for integrity checks prior import as
    done by default in the manual process.

--- virtualbox/vbox-export-snapshots.py
+++ virtualbox/vbox-export-snapshots.py
@@ -10,6 +10,7 @@ import hashlib
 import virtualbox
 from virtualbox.library import VirtualSystemDescriptionType as DescType
 from virtualbox.library import NetworkAttachmentType as NetType
+from virtualbox.library import ExportOptions as ExportOps
 from datetime import datetime
 
 # Base name of the exported VMs
@@ -73,7 +74,7 @@ if __name__ == "__main__":
             sys_description = vm.export_to(appliance, exported_vm_name)
             sys_description.set_final_value(DescType.name, exported_vm_name)
             sys_description.set_final_value(DescType.description, description)
-            progress = appliance.write("ovf-1.0", [], filename)
+            progress = appliance.write("ovf-1.0", [ExportOps.create_manifest], filename)
             print(f"Exporting {filename} (this will take some time, go for an 🍦!)")
             progress.wait_for_completion(-1)
 
